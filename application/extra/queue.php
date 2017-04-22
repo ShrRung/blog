72d61b562e07ce1e890a533c052e49cfe4ce5c3a
ShrRung <1026825079@qq.com>
2017-04-22 23:53:55 +0800

    服务器线上修改

--- application/extra/queue.php
+++ application/extra/queue.php
@@ -1,21 +1,14 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
+// | Author: yunwuxin <448901948@qq.com>
 // +----------------------------------------------------------------------
 
 return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '/' => 'index/index/index',
-    'books' => 'index/index/books',
-    'contact' => 'index/index/contact',
-    'blog' => 'index/index/blog',
-    'about' => 'index/index/about',
+    'connector' => 'Sync'
 ];
